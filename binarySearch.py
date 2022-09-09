0b7a7e61b39b1cbd7bb56557618f1c12346d2efe
Sonola Moyo <sonolamoyosoluwalorun@gmail.com>
2022-09-09 17:27:43 +0100

    DSA Binary Search

---
+++ binarySearch.py
@@ -0,0 +1,34 @@
+"""You're going to write a binary search function.
+You should use an iterative approach - meaning
+using loops.
+Your function should take two inputs:
+a Python list to search through, and the value
+you're searching for.
+Assume the list only has distinct elements,
+meaning there are no repeated values, and 
+elements are in a strictly increasing order.
+Return the index of value, or -1 if the value
+doesn't exist in the list."""
+
+def binary_search(input_array, value):
+    """Your code goes here."""
+    found = False
+    first = 0
+    last = len(input_array) - 1
+    while first <= last and found == False:
+        middleIndex = (first + last)//2
+        if value == input_array[middleIndex]:
+            found = True
+            return middleIndex
+        else:
+            if value > input_array[middleIndex]:
+                first = middleIndex + 1
+            else:
+                last = middleIndex - 1
+    return -1
+
+test_list = [1,3,9,11,15,19,29]
+test_val1 = 25
+test_val2 = 15
+print (binary_search(test_list, test_val1))
+print (binary_search(test_list, test_val2))
