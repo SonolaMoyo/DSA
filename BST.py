8591b3eef8422a0aaa8b241ac42148c48bccf4c5
Sonola Moyo <sonolamoyosoluwalorun@gmail.com>
2022-09-20 14:18:53 +0100

    Binary search tree

--- BST.py
+++ BST.py
@@ -8,11 +8,41 @@ class BST(object):
     def __init__(self, root):
         self.root = Node(root)
 
-    def insert(self, new_val):
-        pass
+    def insert(self, data):
+        if self.root.value:
+            if data < self.root.value:
+                if self.root.left is None:
+                    self.root.left = Node(data)
+                else:
+                    self.root.left = Node(data)
+            elif data > self.root.value:
+                if self.root.right is None:
+                    self.root.right = Node(data)
+                else:
+                    self.root.right = Node(data)
+        else:
+            self.root.value = data
 
     def search(self, find_val):
-        return False
+        """Return True if the value
+        is in the tree, return
+        False otherwise."""
+        values = self.print_tree(self.root)
+        if find_val in values:
+            return True
+        else:
+            return False
+    
+    def print_tree(self, root):
+        """Print out all tree nodes
+        as they are visited in
+        a pre-order traversal."""
+        res = []
+        if root:
+            res.append(root.value)
+            res = res + self.print_tree(root.left)
+            res = res + self.print_tree(root.right)
+        return res
     
 # Set up tree
 tree = BST(4)
